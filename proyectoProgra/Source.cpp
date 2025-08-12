9e25091da7bd68cb875f4115ae4f348ed845bdb1
Josué Duarte <josue.duarte.navarro@est.una.ac.cr>
2025-08-11 19:55:42 -0600

    instalacion smfl

---
+++ proyectoProgra/Source.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(119, 134), "SFML Image Example");
+
+    // Cargando la imagen desde un archivo
+    sf::Texture texture;
+    if (!texture.loadFromFile("image.png")) {
+        return -1; // Error al cargar la imagen
+    }
+
+    // Creando un sprite para mostrar la imagen
+    sf::Sprite sprite(texture);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return 0;
+}
